d4980a08a0abfef3ac5fb1e496069c727502a23f
Matthew Elphick <matt.a.elphy@gmail.com>
2017-01-23 23:07:40 +0000

    :construction: Add basic setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='HHNN',
+  version='1.0',
+  description='A library for making hedonistic hebbian neural networks.',
+  url='https://github.com/maael/HedonisticHebbianNeuralNetwork',
+  download_url='https://github.com/maael/HedonisticHebbianNeuralNetwork',
+  author='Matthew Elphick',
+  author_email='matt.a.elphy@gmail.com',
+  packages=[ 'neuralnetwork' ]
+)
